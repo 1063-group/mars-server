3165eb9bd5c923d9796804b0d2bfb3218b22471e
Bekzodmirzaaliyev <bekzodmirzaaliyev27@gmail.com>
2025-08-02 16:57:37 +0500

    asdada

--- src/models/branch.module.js
+++ src/models/branch.module.js
@@ -1,15 +1,48 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const branchSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    location: [],
-    groups: [
-        
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  district: {
+    type: String,
+    enum: [
+      'Chilonzor', 'Yunusobod', 'Mirobod', 'Yakkasaroy', 'Shayxontohur',
+      'Olmazor', 'Yashnobod', 'Uchtepa', 'Bektemir', 'Sergeli', 'Mirzo Ulugbek',
+      'Yangihayot'
     ],
+    required: true
+  },
+  location: {
+    // Map location (latitude & longitude)
+    type: {
+      lat: { type: Number, required: true },
+      lng: { type: Number, required: true }
+    },
+    required: true
+  },
+  phone: {
+    type: String,
+    default: ''
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+  groups: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Group'
+    }
+  ]
+}, {
+  timestamps: true
+});
 
-})
-
-module.exports = mongoose.model('branch', branchSchema)
+module.exports = mongoose.model('Branch', branchSchema);
